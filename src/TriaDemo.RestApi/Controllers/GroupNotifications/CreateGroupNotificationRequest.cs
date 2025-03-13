179231aaf3cb0277bfa13b54eae6de6b5d882804
Kristijan Popić <kristijan.popic@gmail.com>
2025-03-13 21:30:16 +0100

    Removed unused classes

--- src/TriaDemo.RestApi/Controllers/GroupNotifications/CreateGroupNotificationRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace TriaDemo.RestApi.Controllers.GroupNotifications;
-
-public class CreateGroupNotificationRequest
-{
-    public required string Message { get; set; }
-}
--- src/TriaDemo.RestApi/Controllers/GroupNotifications/CreateGroupNotificationResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace TriaDemo.RestApi.Controllers.GroupNotifications;
-
-public class CreateGroupNotificationResponse
-{
-    public Guid GroupId { get; set; }
-
-    public required string Message { get; set; }
-}
